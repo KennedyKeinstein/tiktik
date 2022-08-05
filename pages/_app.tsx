c93a3cc53ffaf05599e7db9f5757f601d0c339b3
Kennedy <kennedy.developper@gmail.com>
2022-08-05 21:45:01 +0300

    NavBar init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
 
-export default MyApp
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto flex-1 h-[88vh] videos">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyApp;
