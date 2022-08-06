388a9a278c25bc05e2fe9d0b1e443f61491b0695
Kennedy <kennedy.developper@gmail.com>
2022-08-06 08:22:08 +0300

    Navbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
 
-function Sidebar() {
-  return <div>Sidebar</div>;
-}
+const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const toggleSidebar = () => setShowSidebar((prev) => !prev);
+  const normalLink =
+    "flex items-center justify-center gap-3 hover:bg-primary p-3 xl:justify-start cursor:pointer font-semibold text-[#F51997] rounded";
+
+  return (
+    <div>
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-l"
+        onClick={toggleSidebar}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+        {showSidebar && (
+          <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+            <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+              <Link href="/">
+                <div className={normalLink}>
+                  <p className="text-2xl">
+                    <AiFillHome />
+                  </p>
+                  <span className="text-xl hidden xl:block">For you</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
