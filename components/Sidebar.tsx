8dabb0d2c433a3753af2b837d5f3b30ef7840c06
Kennedy <kennedy.developper@gmail.com>
2022-08-06 09:18:59 +0300

    SideBar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 
@@ -8,14 +9,15 @@ const Sidebar = () => {
   const toggleSidebar = () => setShowSidebar((prev) => !prev);
   const normalLink =
     "flex items-center justify-center gap-3 hover:bg-primary p-3 xl:justify-start cursor:pointer font-semibold text-[#F51997] rounded";
+  const userProfile = false;
 
   return (
     <div>
-      <div
-        className="block xl:hidden m-2 ml-4 mt-3 text-l"
-        onClick={toggleSidebar}
-      >
-        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      <div className="block m-2 ml-4 mt-3 text-l" onClick={toggleSidebar}>
+        <div className="xl:hidden">
+          {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+        </div>
+
         {showSidebar && (
           <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
             <div className="xl:border-b-2 border-gray-200 xl:pb-4">
@@ -28,6 +30,32 @@ const Sidebar = () => {
                 </div>
               </Link>
             </div>
+
+            {!userProfile && (
+              <div className="px-2 py-4 xl:block hidden">
+                <p className="text-gray-400">
+                  Log in to like and comment on videos
+                </p>
+
+                <div className="pr-4">
+                  <GoogleLogin
+                    clientId=""
+                    render={(renderProps) => (
+                      <button
+                        className="bg-white text-lg text-[#F51997] border-[#F51997] border-[1px] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997] cursor-pointer"
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                      >
+                        Log in
+                      </button>
+                    )}
+                    onSuccess={() => {}}
+                    onFailure={() => {}}
+                    cookiePolicy="single_host_origin"
+                  />
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
