d0997a24fa9fd7bc94c47099d475274854feddce
Kennedy <kennedy.developper@gmail.com>
2022-08-07 21:50:18 +0300

    Discover

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
 import React from "react";
+import { topics } from "../utils/constants";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  return (
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Popular topics
+      </p>
+
+      <div className="flex gap-3 flex-wrap">
+        {topics.map((item) => (
+          <Link key={item.name} href={`/?topic=${item.name}`}>
+            <div>
+              <span className="font-bold text-2x xl:text-md">{item.icon}</span>
+              <span className="">{item.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
+import Discover from "./Discover";
+import Footer from "./Footer";
+import SuggestedAccounts from "./SuggestedAccounts";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
@@ -56,6 +59,10 @@ const Sidebar = () => {
                 </div>
               </div>
             )}
+
+            <Discover />
+            <SuggestedAccounts />
+            <Footer />
           </div>
         )}
       </div>
