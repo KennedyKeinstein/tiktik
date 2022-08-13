aac333bbffe6b0b1072703ef52ab256f53829f01
Kennedy <kennedy.developper@gmail.com>
2022-08-13 17:08:51 +0300

    post

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useRef, useState } from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { Video } from "../../types";
+import { BASE_URL } from "../../utils";
 
-const Detail = () => {
+interface IProps {
+  postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const router = useRouter();
+  const [post, setPost] = useState(postDetails);
+  const videoRef = useRef();
+
   return <div>Detail</div>;
+};
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: data },
+  };
 };
 
 export default Detail;
