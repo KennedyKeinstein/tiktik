698ca9b7be562a81885e14519ea0c2df1db2ac85
Kennedy <kennedy.developper@gmail.com>
2022-08-13 10:05:30 +0300

    upload video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -67,7 +67,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           onMouseLeave={() => setIsHover(false)}
           className="rounded-3xl"
         >
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               loop
               ref={videoRef}
